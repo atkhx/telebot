9aab91a2ce32fa46e2521625530abe4b48c1ffe5
atkhx <dr.drey.world@icloud.com>
2022-02-26 18:18:36 +0300

    init bot hello world

---
+++ cmd/main/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			if update.Message.Text == "hello" {
+				msg.Text = "fuck u"
+			} else if update.Message.Text == "fuck u to" {
+				msg.Text = "fuck u again"
+			}
+
+			retMsg, retErr := bot.Send(msg)
+
+			log.Println("sent err:", retErr)
+			log.Println("sent msg:", retMsg)
+		}
+	}
+}
